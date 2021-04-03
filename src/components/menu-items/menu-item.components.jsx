6d049e7baa10860e270320cb2a41dd585e32071c
Nate Brate <62619983+natebrate@users.noreply.github.com>
2021-04-03 19:54:34 -0400

    add routing to the pages

--- src/components/menu-items/menu-item.components.jsx
+++ src/components/menu-items/menu-item.components.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss'
 import Image from 'react-bootstrap/Image'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`}
+         onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <Image className='background-image'
                 style={{
                     backgroundImage: `url(${imageUrl})`
-                }} fluid/>
-        {/*<div*/}
-        {/*    className='background-image'*/}
-        {/*    style={{*/}
-        {/*        backgroundImage: `url(${imageUrl})`*/}
-        {/*    }}*/}
-        {/*/>*/}
+                }} fluid
+        />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-)
+);
 
-
-export default MenuItem;
+export default withRouter(MenuItem);

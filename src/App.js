03bdaeb583f6711236f3258d89e8e1b4a4555e21
Nate Brate <62619983+natebrate@users.noreply.github.com>
2021-04-04 16:27:36 -0400

    styling to preview pages and adding shop pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.components";
 
-const HatPage = () => (
-    <div>
-        <h1>Hats are here</h1>
-    </div>
-)
 function App() {
   return (
       <div>
           <div>
           <Switch>
               <Route exact path='/' component={HomePage}/>
-              <Route path='/hats' component={HatPage} />
+              <Route path='/shop' component={ShopPage} />
           </Switch>
           </div>
       </div>
